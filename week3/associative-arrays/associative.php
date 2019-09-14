7985e8802a7b1a46cac0d46b5677b0a3016e2a6b
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-09-13 21:08:58 -0400

    removed comments

--- week3/associative-arrays/associative.php
+++ week3/associative-arrays/associative.php
@@ -1,15 +1,4 @@
 <?php
-/*
-    For this exercise I want you to use php to generate a random number between 1 and 100.
-    After doing this I want the number 50 and greater to be displayed red while numbers below 50 will be displayed blue;
-*/
-
-/*
-1) Create a variable to store the random number.
-2) Find a php function that generates a random number.
-3) Create logic that tests if the reandom number is greater than 50 or not
-4) Within the logic use inline styling to change color of text.
-*/
 
 $profile["first_name"] = "Bob";
 $profile["age"] = 22;
