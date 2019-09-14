83740945e26e9f1c5e8751567dde0ad910eac596
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-09-13 21:21:48 -0400

    practiced data types
    used gettype function to get the data types of five variables

--- week3/data-types/data_types.php
+++ week3/data-types/data_types.php
@@ -2,9 +2,18 @@
 
 $var1 = "bob";
 $var2 = 23;
-$var3 = array["john", 34, false];
+$var3 = array("john", 34, false);
 $var4 = false;
+$var5 = "";
 
-gettype($var1);
+echo gettype($var1);
+echo "<br>";
+echo gettype($var2);
+echo "<br>";
+echo gettype($var3);
+echo "<br>";
+echo gettype($var4);
+echo "<br>";
+echo gettype($var5);
 
 ?>
