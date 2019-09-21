f34098ab0b64b190d90381ddde97398e83f7c6f2
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-09-21 14:23:48 -0400

    changes

--- project 1/form.php
+++ project 1/form.php
@@ -2,5 +2,5 @@
 
 //this script sends an email to captain kirk of the message that the user inputs
 $message = $_POST["message"];
-mail("CaptainKirk@example.com","User's input",$msg);
+mail("CaptainKirk@example.com","User's input",$message);
 ?>
