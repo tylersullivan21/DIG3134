bb421c2a8284f1e65ef88a6e9b10f0599cec5ca9
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-09-21 20:25:20 -0400

    adding original php element for the assignment

--- project 1/form.php
+++ project 1/form.php
@@ -3,4 +3,10 @@
 //this script sends an email to captain kirk of the message that the user inputs
 $message = $_POST["message"];
 mail("CaptainKirk@example.com","User's input",$message);
+
+
+$my_array = array("Dog","Cat","Horse");
+
+list($a, $b, $c) = $my_array;
+echo "I have several animals, a $a, a $b and a $c.";
 ?>
