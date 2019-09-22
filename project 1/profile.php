2fb483a68c40b86bfbf4dcc507dd4277c8942a67
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-09-21 21:22:31 -0400

    got a working php file uses shuffle and count functions

--- project 1/profile.php
+++ project 1/profile.php
@@ -1,8 +1,16 @@
 <?php
 
 $name = $_POST["name"];
+$shoe_brand = $_POST["shoe-brand"];
+$shoe_size = $_POST["shoe-size"];
 
-echo' $_POST["name"]'
 
+$profile = array($name, $shoe_brand, $shoe_size);
+print_r($profile);
+echo "<br>";
+echo  count($profile);
+echo "<br>";
+shuffle($profile);
+print_r($profile);
 
 ?>
