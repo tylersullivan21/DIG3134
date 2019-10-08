24e6d10bacc7797bf5ae192b14eee9fd7e1213db
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-10-07 20:15:36 -0400

    stored all inputs into an array called  $user_entered_input

--- project 1/create_form.php
+++ project 1/create_form.php
@@ -4,6 +4,11 @@ $user_entered_data = array(
 
     "username" => $_POST['username'],
     "password" => $_POST['password'],
+    "email" => $_POST['email'],
+    "address-line1" => $_POST['address-line1'],
+    "city" => $_POST['city'],
+    "zip-code" => $_POST['zip-code'],
+    "state" => $_POST['state'],
     );
 print_r ($user_entered_data);
 
