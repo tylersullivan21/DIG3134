801b29a2414e9afe8c77318b0c1567772f224ee8
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-10-07 22:49:35 -0400

    added regex for zip code

--- project 1/create_form.php
+++ project 1/create_form.php
@@ -14,13 +14,38 @@ $user_entered_data = array(
     /************************** */
     /*THE REGEX FOR USER NAME */
     /************************* */
+
+
 if(preg_match("/[a-z]/",$user_entered_data['username'])){
-    echo 'The regex for the username has been met';
+    echo "The regex for username is matched";
 }
 else{
     echo 'the regex is invalid';
+    }
+
+
+
+    /************************** */
+    /*THE REGEX FOR ZIP CODE */
+    /************************* */
+
+
+    
+if(preg_match("/^\d{5}/", $user_entered_data['zip-code'])){
+
+    echo 'The regex for zip code has been met';
 }
 
+else{
+    echo 'The regex for zip code failed';
+}
+
+
+
+    /*
+validateUsername($user_entered_data['username']);
+validateZip($user_entered_data['zip-code']);
+*/
 print_r ($user_entered_data);
 
 ?>
