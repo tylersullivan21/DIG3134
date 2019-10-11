76dfe283e08ddeb193dd40531997f3e39c6fd8f7
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-10-11 15:37:23 -0400

    added local variables to the validate function

--- project 1/create_form.php
+++ project 1/create_form.php
@@ -1,22 +1,4 @@
 <?php
-header("refresh:5; ./index.html");
-echo "You have sucessfully created an account, " . $_POST['username'] . " You will be directed to the home page in 5 seconds";
-die();
-
-$user_entered_data = array(
-
-    "username" => $_POST['username'],
-    "password" => $_POST['password'],
-    "phone" => $_POST['phone'],
-    "email" => $_POST['email'],
-    "street" => $_POST['street'],
-    "city" => $_POST['city'],
-    "zip-code" => $_POST['zip-code'],
-    "state" => $_POST['state'],
-    );
-
-    //this code cleans out the user input for phone number
-    $user_entered_data['phone'] = preg_replace("/[- \(\)]/", "", $user_entered_data['phone']);
 
 
     /************************** */
@@ -34,17 +16,47 @@ else{
     }
  */
 
-   
+ function redirectOnSuccess (){
+    header("refresh:5; ./index.html");
+    echo "You have sucessfully created an account, " . $_POST['username'] . " You will be directed to the home page in 5 seconds";
+    die();
+ }
+
+
+   function validateInput(){
     /************************** */
     /*THE REGEX FOR ZIP CODE */
     /************************* */
+
+    $user_entered_data = array(
+
+        "username" => $_POST['username'],
+        "password" => $_POST['password'],
+        "phone" => $_POST['phone'],
+        "email" => $_POST['email'],
+        "street" => $_POST['street'],
+        "city" => $_POST['city'],
+        "zip-code" => $_POST['zip-code'],
+        "state" => $_POST['state'],
+        );
+    
+        //this code cleans out the user input for phone number
+        $user_entered_data['phone'] = preg_replace("/[- \(\)]/", "", $user_entered_data['phone']);
+
+    $isValidPhone = false;
+    $isValidAddress = false;
+    $isValidZip = false;
+    $isValidState = false;
+   
 if(preg_match("/^\d{5}$/", $user_entered_data['zip-code'])){
+    $isValidZip = true;
     echo 'The regex for zip code has been met';
     echo "</br>";
 }
 
 else{
-    echo 'The regex for zip code failed';
+    echo 'Your zip code must contain 5 numbers:: '.$user_entered_data['zip-code'] ;
+    echo preg_match("/mustmatch/", "mustmatch");
     echo "</br>";
 }
 
@@ -53,21 +65,23 @@ else{
     /************************* */
 
 if(preg_match("/^[A-Z]{2}$/" , $user_entered_data['state'])){
+    $isValidState = true;
     echo 'The state regex passed';
     echo "</br>";
 }
 
 else{
-    echo 'The regex for state failed';
+    echo 'State must be two capital letters';
     echo "</br>";
 }
 
 if(preg_match("/^\d{1}.*[a-zA-Z]{1}$/" , $user_entered_data['street'])){
+    $isValidAddress = true;
     echo 'The regex for address passed';
     echo "</br>";
 }
 else {
-    echo 'the regex for address failed';
+    echo 'Your street address must start with a number and end with a letter';
     echo "</br>";
 }
 
@@ -76,18 +90,24 @@ else {
     /************************* */
 
     if(preg_match("/^\d{10}$/" , $user_entered_data['phone'])){
+        $isValidPhone = true;
         echo 'the phone number is clean';
         echo "</br>";
     }
     
     else{
-        echo 'The regex forphone number failed';
+        echo 'Your number must be a US number that contains 10 digits';
         echo "</br>";
     }
 
+    //this conditional checks if all flag variables are true or not, when true it will run the success redirect logic.
+    if($isValidAddress === true && $isValidPhone === true && $isValidState === true && $isValidZip === true) {
+        redirectOnSuccess();
+    }
 
+}
 
-
+validateInput();
 
     /*
 validateUsername($user_entered_data['username']);
@@ -96,3 +116,12 @@ validateZip($user_entered_data['zip-code']);
 print_r ($user_entered_data);
 
 ?>
+
+
+
+
+
+
+
+
+
