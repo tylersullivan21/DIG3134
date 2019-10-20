2aac7a73f691439a9fc9590d32c5c67792163c41
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-10-19 23:46:20 -0400

    resolved error messages that were on sulley server

--- dig3134_project2/validate-user.php
+++ dig3134_project2/validate-user.php
@@ -21,11 +21,6 @@ if(isset($_POST) && $_POST['username'] === 'dig3134user' &&  $_POST['password']=
     setcookie("username_cookie", "dig3134",  time() + 60, "/");
     setcookie("password_cookie", "dig3134pass", time() + 60, "/");
     setcookie("login_state", "true", time() + 60);
-    echo $_COOKIE["login_state"];
-    echo $_COOKIE["username_cookie"];
-    echo $_COOKIE["password_cookie"];
-    echo $_COOKIE["username_cookie"];
-    echo $_COOKIE["password_cookie"];
     echo 'Success! The credentials that you entered match our records. You will now be directed...';
     header("refresh:2; ./index.php");
     echo "<br>";
