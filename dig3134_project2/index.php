c756be7e93c626df402ad4e0d6d51e1cf3aa3bb3
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2019-10-20 12:47:58 -0400

    style changes

--- dig3134_project2/index.php
+++ dig3134_project2/index.php
@@ -109,8 +109,8 @@ if (isset($_COOKIE['username_cookie']) &&  isset($_COOKIE['password_cookie']) &&
 //no user logged in SHOW COMMON VIEW
 else {
      echo '
-     <nav>
-    <ul class="nav d-flex justify-content-center align-items-center">
+     <nav class="navbar">
+    <ul class="nav-links d-flex justify-content-center align-items-center">
      <li class="nav-item">
        <a class="nav-link active" href="index.php">Home</a>
      </li>
@@ -121,25 +121,26 @@ else {
        <a class="nav-link" href="contact.html">Contact</a>
      </li>
    </ul>
+   <form class="input-form" action="validate-user.php" method="post">
+
+   <div class="username_input" class="form-field">
+       <label for="username">Username</label>
+       <input type="text" name="username" id="shoe-brand" placeholder="Username" required />
+     </div>
+
+     <div class="password_input" class="form-field">
+       <label for="password">Password</label>
+       <input type="password" name="password" id="shoe-brand" placeholder="Password" required />
+     </div>
+     <div class="submit-btn" class="form-field">
+       <label for="submit"></label>
+       <input class="submit-button" type="submit" value="Submit" name="submit" />
+     </div>
+</form>
    </nav>
-     <link rel="stylesheet" href="css/input-form.css">
-     <form class="input-form" action="validate-user.php" method="post">
-
-     <div class="username_input" class="form-field">
-         <label for="username">Username</label>
-         <input type="text" name="username" id="shoe-brand" placeholder="Username" required />
-       </div>
-
-       <div class="password_input" class="form-field">
-         <label for="password">Password</label>
-         <input type="password" name="password" id="shoe-brand" placeholder="Password" required />
-       </div>
-       <div class="submit-btn" class="form-field">
-         <label for="submit"></label>
-         <input type="submit" value="Submit" name="submit" />
-       </div>
-  </form>
-  <form action="create_form.php" class="create-account-content">
+     <link rel="stylesheet" href="css/input-form.css?v=1.0">
+  
+  <form class="user-button" action="create_form.php" class="create-account-content">
   <p>Dont have an account?</p>
   <label for="submit"></label>
   <input type="submit" value="Create Account" name="submit" />
