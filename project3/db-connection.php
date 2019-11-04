edc563953eabc63fe4e73da8149475a803ab4069
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-04 14:46:17 -0500

    working database connection to sulley server

--- project3/db-connection.php
+++ project3/db-connection.php
@@ -4,7 +4,7 @@ $password = "KumaJake2198!";
 $dbname = "ja240668";
 
 
-$connection = mysqli_connect("localhost", "$username", "$password", "$dbname") or die(mysql_error());
+$connection = mysqli_connect("localhost", "$username", "$password", "$dbname") or die(mysqli_error($connection));
 
 if (!$connection) {
     die("It did not run" . mysqli_connect_error());
@@ -12,13 +12,15 @@ if (!$connection) {
 
     echo "The php code ran";
 }
-
+/*
 $sql = "INSERT INTO project3
 VALUES (4,'thefourth','wordssap','32696')";
 
-mysqli_query($connection, $sql) or die('Could not create table: ' . mysql_error());
+mysqli_query($connection, $sql) or die('Could not create table: ' . mysqli_error($connection));
 
 mysqli_close($connection);
+*/
+
 
 
 ?>
