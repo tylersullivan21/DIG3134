b06b0b5c1e37973ac06b91e9d749b420b1c69263
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-13 15:00:23 -0500

    created a backup for project 3

---
+++ project3_backup/validate-user.php
@@ -0,0 +1,45 @@
+<?php
+include 'db-connection.php';
+/*
+$username = $_POST['username'];
+$password = $_POST['password'];
+*/
+/*
+$isValidUsername = false;
+$isValidPassword = false;
+/*
+if($_POST['username'] === 'dig3134'){
+    $isValidUsername = true;
+}
+else
+*/
+if(isset($_POST))
+{
+$select = "SELECT password FROM project3 WHERE username='".$_POST['username']."'";
+
+/* working on the password logic here*/
+$connection = getConnection();
+$result= mysqli_query($connection, $select) or die('No matching records found' . mysqli_error($connection));
+mysqli_close($connection);
+$row = mysqli_fetch_row($result);
+print_r($row);
+if(sha1($_POST['password'])=== $row[0]){
+    //set the cookie login state to true, store username and passsword and redirect to welcome page
+    /*
+    $isValidUsername = true;
+    $isValidPassword = true; 
+    */
+    session_start();
+   // echo session_id();
+    $_SESSION['loggedin'] ='tyler_true';
+    
+    echo 'Success! The credentials that you entered match our records. You will now be directed...';
+    header("refresh:6; ./index.php");
+    echo "<br>";
+}
+else{
+    header("refresh:5; ./index.php");
+    echo'The credentials that you entered were incorrect, please try again.';
+}
+}
+?>
