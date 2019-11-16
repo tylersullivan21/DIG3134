868a4be64aa2bb9f0b5e55a3acb8bcc9106137fb
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-16 16:14:31 -0500

    created new backup

---
+++ p3_backup/project3/db-connection.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+    $username = "ja240668";
+    $password = "Password2121!";
+    $dbname = "ja240668";
+
+    $connection = mysqli_connect("localhost", "$username", "$password", "$dbname") or die(mysqli_error($connection));
+
+
+    if (!$connection) {
+        die("It did not run" . mysqli_connect_error());
+    } else {
+        return $connection;
+        echo "The php code ran";
+    }
+
+
+
+
+/*
+$sql = "INSERT INTO project3
+VALUES (4,'thefourth','wordssap','32696')";
+
+mysqli_query($connection, $sql) or die('Could not create table: ' . mysqli_error($connection));
+
+mysqli_close($connection);
+*/
+
+
+
+?>
